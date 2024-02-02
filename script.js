03c388599bb80bffd3b4343f554ea55bc6213af9
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2024-02-02 11:40:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // write js code if required
+document.getElementById('flip').addEventListener('mouseover',(e)=>{
+	e.target.src = "https://live.staticflickr.com/8138/29099429912_0659caf55b_b.jpg"
+})
+document.getElementById('flip').addEventListener('mouseleave',(e)=>{
+	e.target.src = "https://images.pexels.com/photos/6785279/pexels-photo-6785279.jpeg"
+})
